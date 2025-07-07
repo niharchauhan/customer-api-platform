3ed6bd6917d3802633be78724c03efa1ea03e60d
niharchauhan <niharchauhan9@gmail.com>
2025-07-06 22:35:03 -0500

    Add Spring Boot main application class

---
+++ customer-api-service/src/main/java/com/example/customer_api_service/CustomerApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.customer_api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApiServiceApplication.class, args);
+	}
+
+}
